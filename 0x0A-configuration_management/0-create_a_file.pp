3a50a99f9bdc968713a9c2779c18955d4e8e7db5
Prosper0200 <agarreyiruoghene@gmail.com>
2024-02-25 07:24:23 -0800

    configure

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This manifest creates a file in /tmp named school with the specified permissions,
+# ownership, and contents.
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# This manifest kills the process named "killmenow"
+exec { 'killmenow':
+  command => '/usr/bin/pkill killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow',
+}
